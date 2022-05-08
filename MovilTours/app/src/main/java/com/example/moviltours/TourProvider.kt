4bcdaf8978c2cf0e2b95e37e0b2f77777ecd814e
sebastiancm95 <58349859+sebastiancm95@users.noreply.github.com>
2022-05-07 23:57:51 -0600

    ,

--- MovilTours/app/src/main/java/com/example/moviltours/TourProvider.kt
+++ MovilTours/app/src/main/java/com/example/moviltours/TourProvider.kt
@@ -7,42 +7,42 @@ class TourProvider {
                 name = "Tamarindo",
                 latitude = 10.3003327,
                 longitude = -85.8425934,
-                price = "15000",
+                price = "₡15000",
                 photo = "https://upload.wikimedia.org/wikipedia/commons/c/cc/Costa_Rica_Playa_Tamarindo_and_Rivermouth_2007_Aerial_Photograph_Tamarindowiki_01.JPG"
             ),
             Tour(
                 name = "Volcán Arenal",
                 latitude = 10.4626259,
                 longitude = -84.7119342,
-                price = "10000",
+                price = "₡10000",
                 photo = "https://upload.wikimedia.org/wikipedia/commons/d/d7/Volcan_Arenal_Costa_Rica_0065.JPG"
             ),
             Tour(
                 name = "Crater la Olla",
                 latitude = 9.9339943,
                 longitude = -83.8569498,
-                price = "7000",
+                price = "₡7000",
                 photo = "https://www.tec.ac.cr/hoyeneltec/sites/default/files/media/uploads/irina-grajales-navarrete/crater_la_olla.jpeg"
             ),
             Tour(
                 name = "Tortuguero",
                 latitude = 10.5445633,
                 longitude = -83.5118866,
-                price = "5000",
+                price = "₡5000",
                 photo = "https://sfo2.digitaloceanspaces.com/elpaiscr/2019/05/Tortuguero-Limon-costa-rica.jpg"
             ),
             Tour(
                 name = "Puerto Viejo",
                 latitude = 9.6529404,
                 longitude = -82.761823,
-                price = "12000",
+                price = "₡12000",
                 photo = "https://h8r3x6j3.rocketcdn.me/wp-content/uploads/2021/11/puerto-viejo-costa-rica-1200x674.jpg"
             ),
             Tour(
                 name = "Cataratas de Nauyaca",
                 latitude = 9.2732268,
                 longitude = -83.8216322,
-                price = "11000",
+                price = "₡11000",
                 photo = "https://www.larepublica.net/storage/images/2019/06/21/20190621111008.nauyaca.jpg"
             )
         )
