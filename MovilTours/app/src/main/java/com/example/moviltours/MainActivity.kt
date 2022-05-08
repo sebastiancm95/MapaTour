77eb00657dac4ce925dde5d904384463e3391c10
JustinVegaMadrigal-UNA <Justin.Vega.Madrigal@est.una.ac.cr>
2022-05-07 22:54:38 -0600

    Map

--- MovilTours/app/src/main/java/com/example/moviltours/MainActivity.kt
+++ MovilTours/app/src/main/java/com/example/moviltours/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moviltours
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviltours.databinding.ActivityMainBinding
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnExplorar.setOnClickListener {
+            val intent = Intent (this, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
