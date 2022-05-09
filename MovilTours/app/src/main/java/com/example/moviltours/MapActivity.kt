00892ddfb420b9f442ea223a482ff9715610e796
JustinVegaMadrigal-UNA <Justin.Vega.Madrigal@est.una.ac.cr>
2022-05-08 19:04:52 -0600

    Ads and map fix

--- MovilTours/app/src/main/java/com/example/moviltours/MapActivity.kt
+++ MovilTours/app/src/main/java/com/example/moviltours/MapActivity.kt
@@ -1,11 +1,13 @@
 package com.example.moviltours
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.moviltours.databinding.ActivityMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
@@ -24,10 +26,25 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.btnVolver.setOnClickListener { onBackPressed() }
         binding.btnTourSugerido.setOnClickListener { createPolyLines() }
+        binding.btnCambiarTipoMapa.setOnClickListener { cambiarTipoMapa () }
         _tour = intent.getSerializableExtra(Test.tour_name) as Tour
+
+        binding.tvTituloMapa.text = _tour.name
+
         createFragment()
+    }
+
+    private fun cambiarTipoMapa () {
+
+        if (map.mapType == GoogleMap.MAP_TYPE_SATELLITE) {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+        }
+        else {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        }
     }
 
     private fun createPolyLines() {
@@ -38,7 +55,16 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         polylineOptions.width(25f)
         polylineOptions.color(ContextCompat.getColor(this, R.color.yellow))
         polylineOptions.add(LatLng(_tour.latitude, _tour.longitude))
-        polylineOptions.add(LatLng(1.0, 1.0))
+
+        if (!_tour.travel.isEmpty()) {
+            for (item in _tour.travel) {
+                polylineOptions.add(item)
+            }
+        }
+        else {
+            Toast.makeText(this, "Este destino no posee un tour planificado", Toast.LENGTH_LONG).show()
+        }
+
         val polyLine = map.addPolyline(polylineOptions)
     }
 
@@ -49,12 +75,13 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap?) {
         map = googleMap!!
+        map.mapType = GoogleMap.MAP_TYPE_SATELLITE
         createMarker()
     }
 
     private fun createMarker() {
         val coordinates = LatLng(_tour.latitude, _tour.longitude)
-        val marker = MarkerOptions().position(coordinates).title("Título de ejemplo")
+        val marker = MarkerOptions().position(coordinates).title(_tour.name)
         map.addMarker(marker)
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 16f), 10000, null)
     }
